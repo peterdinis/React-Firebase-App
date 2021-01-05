f5a151083eb0247e5148df8f3a9bef58aec6eba2
Peter Dinis <peterdinis0@pobox.sk>
2021-01-05 20:15:22 +0100

    Updated code and context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
-      <Signup />
+      <Login />
     </div>
   );
 }
